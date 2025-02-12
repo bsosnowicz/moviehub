5b7bcb5ab3785bae180fb63fa9a2abc103f73c09
bsosnowicz <bartek.sosnowicz@gmail.com>
2025-02-12 21:06:29 +0100

    menu fix, active storage migration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,9 @@ Rails.application.configure do
   # Enable server timing.
   config.server_timing = true
 
+  #google active storage
+  config.active_storage.service = :google
+  
   # Enable/disable Action Controller caching. By default Action Controller caching is disabled.
   # Run rails dev:cache to toggle Action Controller caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
