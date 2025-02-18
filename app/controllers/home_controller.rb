f7a5f6add925dfc2da7e1c9c481fbf38f581c3fe
bsosnowicz <bartek.sosnowicz@gmail.com>
2025-02-18 03:37:58 +0100

    started home redesign

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,31 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @category = params[:category] || "movies"
-    @unreleased_category = params[:unreleased_category] || "movies"
-
-    @q =
-      case @category
-      when "movies"
-        Movie.ransack(params[:q])
-      when "series"
-        Series.ransack(params[:q])
-      end
-
-    @items = @q&.result&.order(rating: :desc)&.limit(5) || []
-
-    @q_unreleased = 
-      case @unreleased_category
-      when "movies"
-        Movie.ransack(params[:q_unreleased])
-      when "series"
-        Series.ransack(params[:q_unreleased])
-      end
-
-    @unreleased_items = @q_unreleased&.result&.where('release_date > ?', Date.today)&.limit(5) || []
-
-    @q_actors =
-      Actor.ransack(params[:q_actors])
-
-    @actors_items = @q_actors&.result&.order(rating: :desc)&.limit(5) || []
+    @q = Movie.ransack(params[:q])
+    @movies = @q.result.order(rating: :desc).limit(4)
   end
 end
