6baa7e4b1e16e1d693c7440bb108d98fa71d0139
bsosnowicz <bartek.sosnowicz@gmail.com>
2025-02-26 21:40:09 +0100

    payment checkout

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,10 +1,9 @@
 class CheckoutsController < ApplicationController
-
   def show
     Rails.logger.debug "Current User: #{current_user.inspect}"
     current_user.set_payment_processor :stripe
     current_user.payment_processor.customer_name
-
+    
     @checkout_session = current_user
       .payment_processor
       .checkout(
@@ -16,6 +15,7 @@ class CheckoutsController < ApplicationController
   end
 
   def success
-
+    @session = Stripe::Checkout::Session.retrieve(params[:stripe_checkout_session_id])
+    @line_items = Stripe::Checkout::Session.list_line_items(params[:stripe_checkout_session_id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   pay_customer stripe_attributes: :stripe_attributes
 
+  alias_attribute :email, :email_address
+
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 
   def stripe_attributes(pay_customer)
